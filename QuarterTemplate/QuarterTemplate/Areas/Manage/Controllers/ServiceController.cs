0b62bcd2ac70ab9793c9d97c0a9716e4bf13c720
ilhambinyetov <binyetov631@gmail.com>
2021-10-12 17:12:32 +0400

    service controller created and crud done

--- QuarterTemplate/QuarterTemplate/Areas/Manage/Controllers/ServiceController.cs
+++ QuarterTemplate/QuarterTemplate/Areas/Manage/Controllers/ServiceController.cs
@@ -4,6 +4,7 @@ using QuarterTemplate.Data;
 using QuarterTemplate.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -47,6 +48,59 @@ namespace QuarterTemplate.Areas.Manage.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Service service = _context.Services.FirstOrDefault(x => x.Id == id);
+            ViewBag.Icons = _context.Services.ToList();
+
+            if (service == null) return NotFound();
+
+            return View(service);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Service service)
+        {
+            if (!ModelState.IsValid) return View();
+
+            Service existService = _context.Services.FirstOrDefault(x => x.Id == service.Id);
+
+            if (existService == null) return NotFound();
+
+
+            existService.Title = service.Title;
+            existService.Description = service.Description;
+            existService.Order = service.Order;
+            existService.Icon = service.Icon;
+            
+
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+
+        public IActionResult DeleteFetch(int id)
+        {
+            Service service = _context.Services.FirstOrDefault(x => x.Id == id);
+
+            if (service == null) return Json(new { status = 404 });
+
+            try
+            {
+                _context.Services.Remove(service);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return Json(new { status = 500 });
+            }
+            
+
+            return Json(new { status = 200 });
+
+
         }
 
     }
